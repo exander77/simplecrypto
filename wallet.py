4418927fb26e1a7c3cd31cb8559cd5658bc449f6
Radomír Polách <rp@t4d.cz>
2021-01-13 17:47:17 +0100

    minor

--- wallet.py
+++ wallet.py
@@ -1,7 +1,7 @@
 import hashlib, sys as S; T=' %s'; U=('%32s : %s\n'*5)[:-1]; B='big'; a=input()
 class ECC: # Secp256k1: https://en.bitcoin.it/wiki/Secp256k1 # ! EXPERIMENTAL !
     def __init__(P, x, y, p): P.x=x; P.y=y; P.p=p # Long live Satoshi Nakamoto.
-    def toXY(P): return b'\x04'+P.x.to_bytes(32, B)+P.y.to_bytes(32, B)
+    def toXY(P): return b'\x04'+P.x.to_bytes(32, B)+P.y.to_bytes(32, B)  # 2021
     def toX(P): return (b'\x03' if P.x%2==1 else b'\x02')+P.x.to_bytes(32, B) #
     def __add__(P, Q): return P.__radd__(Q)#╔═════════════════════════════════╗
     def __mul__(P, x): return P.__rmul__(x)#║──╦╩╩═╩╩══╗───── SimpleCrypto ───║
@@ -13,7 +13,7 @@ class ECC: # Secp256k1: https://en.bitcoin.it/wiki/Secp256k1 # ! EXPERIMENTAL !
     def __radd__(P, Q): # IS EXPERIMENTAL  #║github.com/exander77/simplecrypto║
         if Q is None: return P # !!!!!!!!  #╚═════════════════════════════════╝
         if P==Q: d= 2*P.x; s= pow(2*P.y % P.p, P.p-2, P.p) * (3*P.x ** 2) % P.p
-        else: d= P.x+Q.x; s= pow(Q.x-P.x, P.p-2, P.p) * (Q.y-P.y) % P.p  # 2021
+        else: d= P.x+Q.x; s= pow(Q.x-P.x, P.p-2, P.p) * (Q.y-P.y) % P.p
         x= (s ** 2-d) % P.p; return ECC(x, (s*(P.x-x)-P.y) % P.p, P.p)
 def g(d, r='', A='123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'):
     while d>0: r= A[d % 58]+r; d= d // 58   # Dedicated to Ledger leak victims.
